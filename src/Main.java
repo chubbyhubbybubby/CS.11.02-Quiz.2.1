dfc5a2e59a87aec316549deb52689b42b4c19b23
chubbyhubbybubby <austinc2026@cis.edu.hk>
2023-09-21 08:35:44 +0800

    oh no

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,44 @@
 public class Main {
 
     public static void main(String[] args) {
+         class myclass{
+            public static int add(int numOne, int numTwo) {
+                int resultOne = 19 + 24;
+                System.out.println(resultOne);
+                return numOne + numTwo;
+            }
 
+            public static String greeting(String name) {
+                String resultTwo = "Alice";
+                System.out.println(resultTwo);
+                return "Bonjour, " + name + "!";
+            }
+
+            public static int add(int numOne, int numTwo, int numThree, int numFour) {
+                int resultOne = add( 6 , 12);
+                int resultTwo = add(9, 11);
+
+                return add(resultTwo, numFour);
+            }
+
+            public static void printMe(String toBePrinted) {
+                String ToBePrinted = "hi";
+                System.out.println(ToBePrinted);
+            }
+
+            public static void main(String[] args) {
+                int resultOne = add(19, 24);
+                System.out.println(resultOne);
+
+                String resultTwo = greeting( "Alice");
+                System.out.println(resultTwo);
+
+                int resultThree = add(2, 4, 6, 8);
+                System.out.println(resultThree);
+
+                printMe("Hello!");
+            }
+        }
     }
 
 }
